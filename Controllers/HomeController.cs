6307b78461b677aba4a2523884a0d7fd39927235
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-06-27 09:48:49 +0200

    Updated Bootstrap, addev entreprise view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SemaineDevOps.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index","Entreprises");
         }
 
         public IActionResult Privacy()
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿    let containerCreation = document.getElementById('containerCreation');
 
-// Write your JavaScript code.
+    function createEntreprise(){
+        containerCreation.classList.toggle('active');
+}
+
+
+document.querySelectorAll('tr').forEach(function (row) {
+    var cells = row.querySelectorAll('td');
+    cells.forEach(function (td, index) {
+        if (td.textContent.trim() === '0') {
+            td.classList.add('red');
+            if (index + 3 < cells.length) {
+                cells[index + 1].classList.add('selected');
+            }
+        }
+        else if (td.textContent.trim() === '1') {
+            td.classList.add('yellow');
+            if (index + 2 < cells.length) {
+                cells[index + 3].classList.add('selected');
+            }
+        }
+        else if (td.textContent.trim() === '2') {
+            td.classList.add('green');
+            if (index + 1 < cells.length) {
+                cells[index + 2].classList.add('selected');
+            }
+        }
+    });
+});
